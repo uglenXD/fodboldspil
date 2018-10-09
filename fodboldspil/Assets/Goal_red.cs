a9fbc6a483e08ab52c6b3da4eb4f4b2ed14f8e6b
Simon <simonahtx16@gmail.com>
2018-10-09 15:14:43 +0200

    mål færdig rød

--- fodboldspil/Assets/Goal_red.cs
+++ fodboldspil/Assets/Goal_red.cs
@@ -4,24 +4,52 @@ using UnityEngine;
 
 public class Goal_red : MonoBehaviour {
 
+
+   public int score = 0;
+   public GameObject red0;
+   public GameObject red1;
+   public GameObject red2;
+   public GameObject red3;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        red0.SetActive(true);
+        red1.SetActive(false);
+        red2.SetActive(false);
+        red3.SetActive(false);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
+        print(score);
+
+        if (score == 0)
+        {
+            red0.SetActive(true);
+            red1.SetActive(false);
+            red2.SetActive(false);
+            red3.SetActive(false);
+        }
+        else if (score == 1)
+        {
+            red0.SetActive(false);
+            red1.SetActive(true);
+            red2.SetActive(false);
+            red3.SetActive(false);
+        }
 
 
-		
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Ball")
         {
-            print("Blue is epic");
+            score = score + 1;
+            print(score);
         }
     }
 
