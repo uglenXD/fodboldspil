a9ccfe2320bbadf5a42ede1381ccc46f4a50e4f6
Simon <simonahtx16@gmail.com>
2018-10-12 12:44:14 +0200

    lyd til mål

--- fodboldspil/Assets/Goal_blue.cs
+++ fodboldspil/Assets/Goal_blue.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class Goal_blue : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int scoreb = 0;
+    public GameObject blue0;
+    public GameObject blue1;
+    public GameObject blue2;
+    public GameObject blue3;
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject bold;
+
+    //https://answers.unity.com/questions/1012277/how-to-make-sound-play-only-once-on-trigger.html
+    //lyd tutorial
+    public AudioClip goal1;
+    private AudioSource source;
+
+    //https://gamedev.stackexchange.com/questions/129663/how-to-change-position-of-a-gameobject-from-another-gameobjects-script
+    //Brugt til at reset object position
+
+    // Use this for initialization
+    void Start () {
+
+        source = GetComponent<AudioSource>();
+
+        blue0.SetActive(true);
+        blue1.SetActive(false);
+        blue2.SetActive(false);
+        blue3.SetActive(false);
+
+        player1 = GameObject.Find("Player 1");//This will search for the object by name;
+        player1 = GameObject.FindGameObjectWithTag("player1");
+
+        player2 = GameObject.Find("Player 2");//This will search for the object by name;
+        player2 = GameObject.FindGameObjectWithTag("player2");
+
+        bold = GameObject.Find("Bold");//This will search for the object by name;
+        bold = GameObject.FindGameObjectWithTag("Ball");
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        print(scoreb);
+
+        if (scoreb == 0)
+        {
+            blue0.SetActive(true);
+            blue1.SetActive(false);
+            blue2.SetActive(false);
+            blue3.SetActive(false);
+        }
+        else if (scoreb == 1)
+        {
+            blue0.SetActive(false);
+            blue1.SetActive(true);
+            blue2.SetActive(false);
+            blue3.SetActive(false);
+        }
+        else if (scoreb == 2)
+        {
+            blue0.SetActive(false);
+            blue1.SetActive(false);
+            blue2.SetActive(true);
+            blue3.SetActive(false);
+        }
+        else if (scoreb == 3)
+        {
+            blue0.SetActive(false);
+            blue1.SetActive(false);
+            blue2.SetActive(false);
+            blue3.SetActive(true);
+        }
+    }
 
 
     //tak laurits
@@ -20,7 +83,14 @@ public class Goal_blue : MonoBehaviour {
     {
         if (col.tag == "Ball")
         {
-            print("Red is epic");
+            scoreb = scoreb + 1;
+            print(scoreb);
+            player1.transform.position = new Vector3(24, 0, 0);
+            player2.transform.position = new Vector3(-24, 0, 0);
+            bold.transform.position = new Vector3(0, 0, 0);
+            //source.clip = goal1;
+            //source.Play();
+            source.PlayOneShot (goal1,1);
         }
     }
 }
--- fodboldspil/Assets/Goal_red.cs
+++ fodboldspil/Assets/Goal_red.cs
@@ -10,9 +10,28 @@ public class Goal_red : MonoBehaviour {
    public GameObject red1;
    public GameObject red2;
    public GameObject red3;
+   public GameObject player1;
+   public GameObject player2;
+   public GameObject bold;
 
-	// Use this for initialization
-	void Start () {
+    //https://answers.unity.com/questions/1012277/how-to-make-sound-play-only-once-on-trigger.html
+    //lyd tutorial
+    public AudioClip goal1;
+   private AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+
+        player1 = GameObject.Find("Player 1");//This will search for the object by name;
+        player1 = GameObject.FindGameObjectWithTag("player1");
+
+        player2 = GameObject.Find("Player 2");//This will search for the object by name;
+        player2 = GameObject.FindGameObjectWithTag("player2");
+
+        bold = GameObject.Find("Bold");//This will search for the object by name;
+        bold = GameObject.FindGameObjectWithTag("Ball");
+
+        source = GetComponent<AudioSource>();
 
         red0.SetActive(true);
         red1.SetActive(false);
@@ -40,7 +59,20 @@ public class Goal_red : MonoBehaviour {
             red2.SetActive(false);
             red3.SetActive(false);
         }
-
+        else if (score == 2)
+        {
+            red0.SetActive(false);
+            red1.SetActive(false);
+            red2.SetActive(true);
+            red3.SetActive(false);
+        }
+        else if (score == 3)
+        {
+            red0.SetActive(false);
+            red1.SetActive(false);
+            red2.SetActive(false);
+            red3.SetActive(true);
+        }
 
     }
 
@@ -48,8 +80,13 @@ public class Goal_red : MonoBehaviour {
     {
         if (col.tag == "Ball")
         {
+            player1.transform.position = new Vector3(24, 0, 0);
+            player2.transform.position = new Vector3(-24, 0, 0);
+            bold.transform.position = new Vector3(0, 0, 0);
+
             score = score + 1;
             print(score);
+            source.PlayOneShot(goal1, 1);
         }
     }
 
